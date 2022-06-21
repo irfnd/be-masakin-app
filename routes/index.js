e21fcb7c1469692eb556c6adfac59ef1ad28c5a8
Irfnd <irfandiabimanyu@gmail.com>
2022-06-21 21:17:16 +0700

    Update - Add page not found route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
+const { responseError } = require("../libs/responseFormat/response");
+
 module.exports = (app) => {
   app.use("/users", require("./route.users"));
   app.use("/recipes", require("./route.recipes"));
   app.use("/comments", require("./route.comments"));
   app.use("/liked-recipes", require("./route.liked_recipes"));
+
+  app.use("*", (req, res) =>
+    res.status(404).json(responseError("Page route not found!"))
+  );
 };
