44a3b04abbd535c901c2425441cac62f43ebed59
Irfnd <irfandiabimanyu@gmail.com>
2022-07-14 13:52:17 +0700

    Update - Fix DB_URI for connection

--- models/connection.js
+++ models/connection.js
@@ -1,15 +1,9 @@
 require("dotenv").config();
-const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } = process.env;
+const { DB_URI } = process.env;
 
 const { Pool } = require("pg");
 
-const db = new Pool({
-	host: DB_HOST,
-	port: DB_PORT,
-	user: DB_USER,
-	password: DB_PASS,
-	database: DB_NAME,
-});
+const db = new Pool({ DB_URI });
 
 db.on("error", (err) => {
 	console.error("> Unexpected error on idle client!\n", err);
