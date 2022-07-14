9da57dcd82f821074c7b4865ac5495667d308b11
Irfnd <irfandiabimanyu@gmail.com>
2022-07-14 14:28:55 +0700

    Update - Fixing base route + add host client for cors

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 require("dotenv").config();
-const { SERVER_HOST, SERVER_PORT } = process.env;
+const { CLIENT_HOST } = process.env;
 
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const port = process.env.PORT || 8000;
 const { handlingError } = require("./middlewares");
 
 const app = express();
 
-app.use(cors({ origin: "http://resip-app.herokuapp.com" }));
+app.use(cors({ origin: CLIENT_HOST }));
 app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }));
 app.use(morgan("dev"));
 app.use(express.json());
@@ -19,6 +20,6 @@ app.use(express.static("public"));
 require("./routes")(app);
 app.use(handlingError);
 
-app.listen(SERVER_PORT, () => {
-	console.log(`> Server running on http://${SERVER_HOST}:${SERVER_PORT}`);
+app.listen(port, () => {
+	console.log(`> Server running successfully`);
 });
