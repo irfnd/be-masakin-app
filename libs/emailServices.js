02d7e798dd0653f8a26de9e81235f25d9f150b1b
Irfandi Iqbal Abimanyu <irfandiabimanyu@gmail.com>
2022-09-02 13:54:00 +0700

    [Update] - Fix url on emailServices.js

--- libs/emailServices.js
+++ libs/emailServices.js
@@ -20,7 +20,7 @@ const sendEmail = async (to, subject, text) => {
 
 const sendVerificationEmail = async (to, token) => {
 	const subject = "Email Verification - Masakin App";
-	const verificationEmailUrl = `http://${serverUrl}/verify?token=${token}`;
+	const verificationEmailUrl = `http://${serverUrl}/auth/verify?token=${token}`;
 	const text = `Dear user,
 To verify your email, click on this link: ${verificationEmailUrl}
 If you did not create an account, then ignore this email.`;
